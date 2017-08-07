a7510210f597be71af154e6ec6558009ec507bf8
Patrick Ian M. Co <patrickianco@outlook.com>
2017-08-07 19:26:48 +0800

    asd

--- app.js
+++ app.js
@@ -236,10 +236,11 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 					var emailCont = `Name of Applicant: ${applicantName}
 Applicant's Email: ${applicantEmail}
 Applicant's Contact Number: ${applicantNum}`;
-				}
-				if(applicantName != null && applicantNum != null && applicantEmail != null){
+					if(applicantName != null && applicantNum != null && applicantEmail != null){
 					sendEmailApplicationRider("Rider Applicant", emailCont, clientName)
 				}
+				}
+				
 				return contextObject;
 			});
 			sendTextMessage(sender, responseText);
