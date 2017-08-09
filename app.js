df7b295b9864837b5830a51c2aafe353332b23f0
Patrick Ian M. Co <patrickianco@outlook.com>
2017-08-09 13:02:14 +0800

    sd

--- app.js
+++ app.js
@@ -304,7 +304,7 @@ Applicant's Contact Number: ${applicantNum1}`;
 		break;
 
 		case 'coupons-action':
-			sendTextMessage(sender, `Hi ${name}, thank you for contacting honestbee! Your First Time User coupon is automatically applied to your first order. You can also find it in your account wallet.`);
+			sendTextMessage(sender, `Hi ${fname}, thank you for contacting honestbee! Your First Time User coupon is automatically applied to your first order. You can also find it in your account wallet.`);
 			sendTextMessage(sender, 'For more information on how to access your account wallet, kindly click this link http://blog.honestbee.com/how-to-use-a-coupon-code-on-honestbee/');
 		break;
 
