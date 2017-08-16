31f432355e9ec4f36afc4bfdc106764607992e47
RomeDorado <romedorado@gmail.com>
2017-08-16 11:04:28 +0800

    try

--- app.js
+++ app.js
@@ -193,11 +193,11 @@ function handleEcho(messageId, appId, metadata) {
 	console.log("Received echo for message %s and app %d with metadata %s", messageId, appId, metadata);
 }
 
-var clientName = "";
-var fname = "";
-var place = "";
+// var clientName = "";
+// var fname = "";
+// var place = "";
 var counter = 0;
-function handleApiAiAction(sender, action, responseText, contexts, parameters) {
+function handleApiAiAction(sender, action, responseText, contexts, parameters, clientName, fname, place) {
 	request({
 		uri: 'https://graph.facebook.com/v2.7/' + sender,
 		qs: {
@@ -507,7 +507,9 @@ function handleApiAiResponse(sender, response) {
 	let contexts = response.result.contexts;
 	let parameters = response.result.parameters;
 	var counter = 0;
-
+	var clientName = "";
+	var fname = "";
+	var place = "";
 	sendTypingOff(sender);
 
 	if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1) && action != "input.unknown"
@@ -518,7 +520,7 @@ function handleApiAiResponse(sender, response) {
 		let timeout = 0;
 		
 
-		handleApiAiAction(sender, action, responseText, contexts, parameters);
+		handleApiAiAction(sender, action, responseText, contexts, parameters, clientName, fname, place);
 		for (var i = 0; i < messages.length; i++) {
 
 			if ( previousType == 1 && (messages[i].type != 1 || i == messages.length - 1)) {
@@ -549,7 +551,7 @@ function handleApiAiResponse(sender, response) {
 		sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
 	} else if (isDefined(action)) {
 		console.log('this is the action' + action);
-		handleApiAiAction(sender, action, responseText, contexts, parameters);
+		handleApiAiAction(sender, action, responseText, contexts, parameters, clientName, fname, place);
 	} else if (isDefined(responseData) && isDefined(responseData.facebook)) {
 		try {
 			console.log('Response as formatted message' + responseData.facebook);
